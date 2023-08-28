51b6965f2bb25efd64f77a4acd306aa06a91bf6c
조연수 <cys@innogrid.com>
2023-08-28 16:22:01 +0900

    refactor : 검색창 전체 포함과 미포함 나눠서 수정

--- src/Custom/CustomSelect/CustomSelect.js
+++ src/Custom/CustomSelect/CustomSelect.js
@@ -1,22 +1,37 @@
 import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import "./CustomSelect.css";
-export default function CustomSelect({inputLabel, placeholder, id, label, onChange, value, data}){
+export default function CustomSelect({inputLabel, placeholder, id, label, onChange, value, data, view}){
     return (
-        <FormControl sx={{width:500, height:50}}>
+        <FormControl>
             <InputLabel>{inputLabel}</InputLabel>
-            <Select
-                className="select"
-                placeholder={placeholder}
-                id={id}
-                label={label}
-                onChange={onChange}
-                value={value}>
-                {data?.map((value)=>{
-                    return <MenuItem value={value.name}>
-                        {value.name}
-                    </MenuItem>
-                })}
-            </Select>
+            {view? (
+                    <Select
+                        placeholder={placeholder}
+                        id={id}
+                        label={label}
+                        onChange={onChange}
+                        value={value}>
+                        <MenuItem value={view}>{view}</MenuItem>
+                        {data?.map((value)=>{
+                            return <MenuItem value={value.name}>
+                                {value.name}
+                            </MenuItem>
+                        })} </Select>
+                )
+                : (
+                    <Select placeholder={placeholder}
+                            id={id}
+                            label={label}
+                            onChange={onChange}
+                            value={value}>
+                        {data?.map((value)=>{
+                            return <MenuItem value={value.name}>
+                                {value.name}
+                            </MenuItem>
+                        })}
+                    </Select>
+                )
+            }
         </FormControl>
     );
 }
