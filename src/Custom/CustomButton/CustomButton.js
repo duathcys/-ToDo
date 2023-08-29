cda205e0c0fa76e22a601260c2344176da85a311
조연수 <cys@innogrid.com>
2023-08-29 18:44:20 +0900

    refactor: 버튼 가운데 정렬

--- src/Custom/CustomButton/CustomButton.js
+++ src/Custom/CustomButton/CustomButton.js
@@ -3,16 +3,21 @@ import "./CustomButton.css";
 
 export default function CustomButton({onClick, name}) {
     return (
-        <Button onClick={onClick} style={
-            {
-                color:"black",
-                fontFamily:"HakgyoansimWoojuR, sans-serif",
-                fontSize: "18px",
-                alignItems: "center",
-                justifyContent: "center",
-            }
-        }>
-            {name}
-        </Button>
+        <div style={{display: "flex", alignItems: "center",
+            justifyContent: "center", marginTop: "20px"}}>
+
+            <Button onClick={onClick} style={
+                {
+                    color:"black",
+                    fontFamily:"HakgyoansimWoojuR, sans-serif",
+                    fontSize: "18px",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }
+            }>
+                {name}
+            </Button>
+        </div>
+
     );
 };
